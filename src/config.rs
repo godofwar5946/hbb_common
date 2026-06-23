63cef70a66840afbe6e902cfc71e7e29538b2b7e
godofwar5946 <126454508+godofwar5946@users.noreply.github.com>
2026-06-23 13:33:18 +0800

    修改服务器地址

--- src/config.rs
+++ src/config.rs
@@ -117,8 +117,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rustdesk2.godofwar5946.top"];
+pub const RS_PUB_KEY: &str = "lCCFCoWgabK0EUSceSEAIGCMY6+I33ls5W7arCp5f3w=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
